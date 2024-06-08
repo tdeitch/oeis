ba79ac9657e80fa56c556a2b7a84ac2b2938d9a7
Trey Deitch <me@tdeitch.com>
2024-06-08 17:29:28 -0400

    Compact

--- a007947.py
+++ a007947.py
@@ -1,12 +1,8 @@
-from math import prod
-from sympy import primefactors
-
-def a007947(n):
-    return  prod(primefactors(n))
+from sympy import primefactors, prod
+def a(n): return  prod(primefactors(n))
 
 if __name__ == "__main__":
-    assert [a007947(i) for i in range(1,13)] == [1,2,3,2,5,6,7,2,3,10,11,6]
-
+    assert [a(i) for i in range(1,13)] == [1,2,3,2,5,6,7,2,3,10,11,6]
     for i in range(1, 10_001):
-        print(f"{i} {a007947(i)}")
+        print(f"{i} {a(i)}")
 
--- a060735.py
+++ a060735.py
@@ -1,17 +1,10 @@
-from sympy import primefactors, prod
-def a(n): return 1 if n == 1 else a(n-1) + prod(primefactors(a(n-1)))
-
 from functools import cache
-
+from sympy import primefactors, prod
 @cache
-def a060735(n):
-    if n == 1: return 1
-    return a060735(n-1) + prod(primefactors(a060735(n-1)))
+def a(n): return 1 if n == 1 else a(n-1) + prod(primefactors(a(n-1)))
 
 if __name__ == "__main__":
     assert [a(n) for n in range(1,13)] == [1,2,4,6,12,18,24,30,60,90,120,150]
-    assert [a060735(n) for n in range(1,13)] == [1,2,4,6,12,18,24,30,60,90,120,150]
-
     for i in range(1, 10_001):
-        print(f"{i} {a060735(i)}")
+        print(f"{i} {a(i)}")
 
