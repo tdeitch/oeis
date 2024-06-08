2df4232fe1afdb56aeed7ec53679e451ea22a361
Trey Deitch <me@tdeitch.com>
2024-06-08 18:08:09 -0400

    Prepare for upload to OEIS

--- a060735.py
+++ a060735.py
@@ -1,10 +1,11 @@
 from functools import cache
 from sympy import primefactors, prod
 @cache
-def a(n): return 1 if n == 1 else a(n-1) + prod(primefactors(a(n-1)))
+def a(n): return 1 if n == 0 else a(n-1) + prod(primefactors(a(n-1)))
+print([a(n) for n in range(42)])
 
-if __name__ == "__main__":
-    assert [a(n) for n in range(1,13)] == [1,2,4,6,12,18,24,30,60,90,120,150]
-    for i in range(1, 10_001):
-        print(f"{i} {a(i)}")
+#if __name__ == "__main__":
+#    assert [a(n) for n in range(1,13)] == [1,2,4,6,12,18,24,30,60,90,120,150]
+#    for i in range(1, 20_001):
+#        print(f"{i} {a(i)}")
 
